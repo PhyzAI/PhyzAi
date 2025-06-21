867dcc42d64b9007a7186dc2a8cb35c466d7bad4
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-06-21 13:29:16 -0700

    start over

--- Phyz.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\User\Desktop\PhyzAi
-python chatbotCurrent.py
--- providers/provider_types.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol
-
-
-class ResponseProvider(Protocol):
-    async def __call__(self, system_prompt: str, user_prompt: str) -> str:
-        ...
