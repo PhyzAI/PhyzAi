587be259961c371a20e67e7ecec5cae03fb4ebb9
Yodaman07 <ayaan.irshad@gmail.com>
2026-06-17 11:52:00 -0700

    basic filter added, gets sent to slm for further judgement if it doesn't pass the manual filter

--- actual_SLM.py
+++ actual_SLM.py
@@ -7,6 +7,8 @@ with open("slm_prompt.txt", "r") as f:
     sysprompt += f.read()
 messages = [{'role': 'system', 'content': sysprompt}]
 
+remembering_keywords = ["I like", "I want", "I have", "My favorite", "I am"] #simple manual filter for basic questions
+
 def should_remember(prompt: str, memory_context=None) -> bool:
     # Factors to consider: is the information in the prompt something that can be searched on the internet?
     # Does the statement contain the word "I" in it (I have, I like, I want, etc.)
@@ -14,25 +16,25 @@ def should_remember(prompt: str, memory_context=None) -> bool:
     # If Phyz knows who they are talking to, remember the data under that user's name, if not, remember under the "guest" username
     # Can use vision/face recognition to identify faces
 
+    if any(word in prompt for word in remembering_keywords): #if they don't pass this, then pass it to the slm
+        return True
+
     messages.append({'role': 'user', 'content': f"\"{prompt}\""})
     response = chat(
         model='smollm2',
         messages=messages,
-        options={temperature: 0.5}
+        # options={temperature: 0.5}
     )
 
     response2 = chat(
         model='llama3.1:8b',
         messages=messages,
-        options={temperature: 0.5}
+        # options={temperature: 0.5}
     )
 
     print(response.message.content)
     print(response2.message.content)
-    # return (
-    #     "yes" in response.message.content.lower()
-    # )
 
 
 if __name__ == "__main__":
-    print(should_remember("I like apples"))
+    print(should_remember("I really love bananas"))
