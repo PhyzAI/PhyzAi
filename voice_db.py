772f57195b03e76ab1acb97d7471e7f0f91cf690
Yodaman07 <ayaan.irshad@gmail.com>
2026-08-01 09:19:03 -0700

    trying to improve the encoder but its a mess

--- voice_db.py
+++ voice_db.py
@@ -1,17 +1,23 @@
-import sys
-from email.mime import audio
+import torch
+# SpeechBrain autocast patch for PyTorch on CPU/macOS
+# if not hasattr(torch.amp, "custom_fwd"):
+#     torch.amp.custom_fwd = torch.cuda.amp.custom_fwd
+# if not hasattr(torch.amp, "custom_bwd"):
+#     torch.amp.custom_bwd = torch.cuda.amp.custom_bwd
 
-from resemblyzer import VoiceEncoder, preprocess_wav
-from pathlib import Path
+import torchaudio
+import torch.nn.functional as F
+from speechbrain.inference.speaker import EncoderClassifier
+
 import numpy as np
 import json
 import os
 
-from sklearn import svm
+# from sklearn import svm
 
 import memory_db
 from transcriber import record_until_silence, save_wav
-from sklearn.svm import SVC
+# from sklearn.svm import SVC
 
 
 
@@ -41,36 +47,30 @@ class Voice_DB:
             json.dump(self.embeds, f, ensure_ascii=False, indent=2)
 
 
-    # def add_voice(self, voice_sample_path, subindex=0):
-    #
-    #     print(voice_sample_path)
-    #     print(subindex)
-    #     fpath = Path(voice_sample_path)
-    #     wav = preprocess_wav(fpath)
-    #     encoder = VoiceEncoder()
-    #     embed = encoder.embed_utterance(wav)
-    #     #you cant json serialize a numpy array
-    #     self.embeds[subindex].append({"embed": embed.tolist(), "speaker": Path(voice_sample_path).stem})
-    #     self._save()
-
     def add_all(self, path="data/known_voices"):
+        classifier = EncoderClassifier.from_hparams(
+            source="speechbrain/spkrec-ecapa-voxceleb"
+        )
+
+        user_embeds = [] # TODO reorder this
         for dir in os.listdir(path):
             directory = os.listdir(path + "/" + dir)
             directory.sort()
-            user_embeds = []
             for file in directory:
                 voice_sample_path = path + "/" + dir + "/" + file # step 1, generate embedding per user sample
-                fpath = Path(voice_sample_path)
+                print(f"Loading from {voice_sample_path}")
+                signal, fs = torchaudio.load(voice_sample_path)
 
-                wav = preprocess_wav(fpath)
-                encoder = VoiceEncoder()
-                embed = encoder.embed_utterance(wav)
+                embedding = classifier.encode_batch(signal)
+                # print(embedding)
+                user_embeds.append(embedding)
 
-                user_embeds.append(embed)
 
-            final_embed = np.mean(np.stack(user_embeds, axis=0), axis=0)
+            final_embed = torch.stack(user_embeds, dim=0).mean(dim=0)
+            # final_embed = np.mean(np.stack(user_embeds_np, axis=0), axis=0)
             self.embeds.append({"embed": final_embed.tolist(), "speaker": dir})
             print(f"generated embeddings for user: {dir}")
+
         self._save()
         print("generated all embeddings")
 
@@ -80,37 +80,43 @@ class Voice_DB:
         if audio is None: return None # if timeout is hit
         self._load()
         #embed the just spoken audio
-        encoder = VoiceEncoder()
-        wave_array = np.frombuffer(audio, dtype=np.int16)
 
+        wave_array = np.frombuffer(audio, dtype=np.int16)
         if wave_array.dtype == np.int16:
             wave_array = wave_array.astype(np.float32) / 32768.0 #converts the pcm wave data to a float32 for resemblyzer
+        wave_tensor = torch.from_numpy(wave_array).float() # convert from numpy
 
-        wav = preprocess_wav(wave_array, source_sr=16000)
-        embed = encoder.embed_utterance(wav)
+        classifier = EncoderClassifier.from_hparams(
+            source="speechbrain/spkrec-ecapa-voxceleb"
+        )
+        embedding = classifier.encode_batch(wave_tensor).squeeze()
 
         for user in self.embeds:
-            score = memory_db.MemoryDB._cosine_similarity(user["embed"], embed)
+            # score = memory_db.MemoryDB._cosine_similarity(user["embed"], embedding.tolist())
+            saved = torch.as_tensor(user["embed"], dtype=torch.float32).squeeze()
+            current = torch.as_tensor(embedding, dtype=torch.float32).squeeze()
+            score = F.cosine_similarity(saved, current, dim=0).item()
+
             print(f"Score: {score}")
             if score >= self.comparison_threshold:
                 return user["speaker"]
         return None
 
-    def svm(self, current, audio2check):
-        # X_train: Matrix of embeddings from your enrolled users + background "imposter" voices
-        # y_train: Labels corresponding to the speaker IDs
-        X_train = np.array([current[0]["embed"], current[1]["embed"], current[2]["embed"], current[3]["embed"], current[4]["embed"]])
-        y_train = np.array([1, 1, 1, 0, 0])  # 1 = Target User, 0 = Someone Else
-        # these are pre sorted now
-
-        # Train a Linear SVM with probability outputs enabled
-        clf = SVC(kernel='linear', probability=True)
-        clf.fit(X_train, y_train)
-
-        # Test a completely new embedding vector
-        # predict_proba returns [Probability of being an imposter, Probability of being Target]
-        confidence_scores = clf.predict_proba([audio2check])[0]
-        print(f"Confidence that this is the target speaker: {confidence_scores[1] * 100:.2f}%")
+    # def svm(self, current, audio2check):
+    #     # X_train: Matrix of embeddings from your enrolled users + background "imposter" voices
+    #     # y_train: Labels corresponding to the speaker IDs
+    #     X_train = np.array([current[0]["embed"], current[1]["embed"], current[2]["embed"], current[3]["embed"], current[4]["embed"]])
+    #     y_train = np.array([1, 1, 1, 0, 0])  # 1 = Target User, 0 = Someone Else
+    #     # these are pre sorted now
+    #
+    #     # Train a Linear SVM with probability outputs enabled
+    #     clf = SVC(kernel='linear', probability=True)
+    #     clf.fit(X_train, y_train)
+    #
+    #     # Test a completely new embedding vector
+    #     # predict_proba returns [Probability of being an imposter, Probability of being Target]
+    #     confidence_scores = clf.predict_proba([audio2check])[0]
+    #     print(f"Confidence that this is the target speaker: {confidence_scores[1] * 100:.2f}%")
 
 
     def collect_voices(self, username, training_count, gen_embeds=False): # its best to collect voices using different mics, and different angles - see voice_training.txt for training lines
@@ -130,9 +136,9 @@ class Voice_DB:
 
 
 if __name__ == "__main__":
-    vdb = Voice_DB()
+    # vdb = Voice_DB()
     # vdb.add_all()
 
-    # audio = record_until_silence()  # seconds
-    # voice_db = Voice_DB()
-    # print(voice_db.find_speaker(audio))  # print the speaker
+    audio = record_until_silence()  # seconds
+    voice_db = Voice_DB()
+    print(voice_db.find_speaker(audio))  # print the speaker
